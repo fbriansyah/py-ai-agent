558162bc8108e222950e9d83ed5cbccd917b37c8
fbriansyah <fbriansyah@gmail.com>
2025-02-07 22:56:19 +0700

    add bruno, env, database

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from py-ai-agent!")
+import uvicorn
+import logging
+# setup fastapi
+from fastapi import FastAPI
 
+# load the config from dot env file
+from dotenv import load_dotenv, get_key
+load_dotenv()
+
+app = FastAPI()
+# setup logging
+logging.basicConfig(level=logging.INFO)
+@app.get("/")
+def read_root():
+    return {"app_name": "AI Agent", "version": "0.0.1"}
+
+def main():
+    port = get_key(".env", "PORT")
+    if port is None:
+        port = 8000
+    else:
+        port = int(port)
+    logging.info("Starting Service at port %s", port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
