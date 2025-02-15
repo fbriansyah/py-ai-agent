5ccf5651033db1e0e98c5fd5caab02548c0cc07a
fbriansyah <fbriansyah@gmail.com>
2025-02-15 15:55:53 +0700

    add mongo database

---
+++ databases/mongo.py
@@ -0,0 +1,30 @@
+
+from pymongo import AsyncMongoClient
+# from pymongo.server_api import ServerApi
+from openai import AsyncOpenAI
+
+class MongoClient:
+    open_ai_client: AsyncOpenAI | None
+    db_name: str
+    def __init__(self, uri: str, db_name: str):
+        self.client = AsyncMongoClient(uri)
+        self.db_name = db_name
+    def setup_openai(self, client: AsyncOpenAI):
+        self.open_ai_client = client
+        
+    def get_database(self, database_name: str):
+        return self.client[database_name]
+    
+    def get_collection(self, collection_name: str):
+        db = self.client[self.db_name]
+        return db[collection_name]
+
+    def ping(self):
+        try:
+            # Send a ping to confirm a successful connection
+            self.client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+
+
